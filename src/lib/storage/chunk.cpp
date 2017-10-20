7ce581b78cf92393ff136e0ca29a4cc08b3714c2
Adrian Holfter <adrian.holfter@student.hpi.uni-potsdam.de>
2017-10-20 16:02:44 +0200

    Finish sprint 1, step 3

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -14,27 +14,29 @@
 
 namespace opossum {
 
-void Chunk::add_column(std::shared_ptr<BaseColumn> column) {
-  // Implementation goes here
-}
+void Chunk::add_column(std::shared_ptr<BaseColumn> column) { _columns.push_back(column); }
 
 void Chunk::append(std::vector<AllTypeVariant> values) {
-  // Implementation goes here
+  Assert(values.size() == _columns.size(), "Data row does not match column layout");
+  for (size_t i_column = 0; i_column < _columns.size(); ++i_column) {
+    _columns[i_column]->append(values[i_column]);
+  }
 }
 
 std::shared_ptr<BaseColumn> Chunk::get_column(ColumnID column_id) const {
-  // Implementation goes here
-  return nullptr;
+  Assert(column_id < _columns.size(), "Column index out of bounds");
+  return _columns[column_id];
 }
 
-uint16_t Chunk::col_count() const {
-  // Implementation goes here
-  return 0;
-}
+uint16_t Chunk::col_count() const { return _columns.size(); }
 
 uint32_t Chunk::size() const {
-  // Implementation goes here
-  return 0;
+  if (_columns.size() == 0) {
+    return 0;
+  }
+
+  DebugAssert(_columns.front(), "There should be a column, but it is null");
+  return _columns.front()->size();
 }
 
 }  // namespace opossum
