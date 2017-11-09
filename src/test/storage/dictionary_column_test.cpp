96fc82ba982071994630c5b7605a5e020742b7ca
Adrian Holfter <adrian.holfter@student.hpi.uni-potsdam.de>
2017-11-09 15:00:35 +0100

    add two tests for DictionaryColumn

--- src/test/storage/dictionary_column_test.cpp
+++ src/test/storage/dictionary_column_test.cpp
@@ -7,6 +7,7 @@
 #include "../../lib/storage/base_column.hpp"
 #include "../../lib/storage/dictionary_column.hpp"
 #include "../../lib/storage/value_column.hpp"
+#include "../../lib/type_cast.hpp"
 
 class StorageDictionaryColumnTest : public ::testing::Test {
  protected:
@@ -51,6 +52,50 @@ TEST_F(StorageDictionaryColumnTest, LowerUpperBound) {
 
   EXPECT_EQ(dict_col->lower_bound(15), opossum::INVALID_VALUE_ID);
   EXPECT_EQ(dict_col->upper_bound(15), opossum::INVALID_VALUE_ID);
+}
+
+TEST_F(StorageDictionaryColumnTest, AccessMethods) {
+  vc_str->append("Bill");
+  vc_str->append("Steve");
+  vc_str->append("Alexander");
+  vc_str->append("Steve");
+  vc_str->append("Hasso");
+  vc_str->append("Bill");
+
+  auto col = opossum::make_shared_by_column_type<opossum::BaseColumn, opossum::DictionaryColumn>("string", vc_str);
+  auto dict_col = std::dynamic_pointer_cast<opossum::DictionaryColumn<std::string>>(col);
+
+  // Check access with [] operator returning an AllTypeVariant
+  EXPECT_EQ(opossum::type_cast<std::string>((*dict_col)[0]), "Bill");
+  EXPECT_EQ(opossum::type_cast<std::string>((*dict_col)[1]), "Steve");
+  EXPECT_EQ(opossum::type_cast<std::string>((*dict_col)[2]), "Alexander");
+  EXPECT_EQ(opossum::type_cast<std::string>((*dict_col)[3]), "Steve");
+  EXPECT_EQ(opossum::type_cast<std::string>((*dict_col)[4]), "Hasso");
+  EXPECT_EQ(opossum::type_cast<std::string>((*dict_col)[5]), "Bill");
+
+  // Check access with get method
+  EXPECT_EQ(dict_col->get(0), "Bill");
+  EXPECT_EQ(dict_col->get(1), "Steve");
+  EXPECT_EQ(dict_col->get(2), "Alexander");
+  EXPECT_EQ(dict_col->get(3), "Steve");
+  EXPECT_EQ(dict_col->get(4), "Hasso");
+  EXPECT_EQ(dict_col->get(5), "Bill");
+
+  // Check access via attribute vector and dictionary
+  auto attribute_vector = dict_col->attribute_vector();
+  EXPECT_EQ(dict_col->value_by_value_id(attribute_vector->get(0)), "Bill");
+  EXPECT_EQ(dict_col->value_by_value_id(attribute_vector->get(1)), "Steve");
+  EXPECT_EQ(dict_col->value_by_value_id(attribute_vector->get(2)), "Alexander");
+  EXPECT_EQ(dict_col->value_by_value_id(attribute_vector->get(3)), "Steve");
+  EXPECT_EQ(dict_col->value_by_value_id(attribute_vector->get(4)), "Hasso");
+  EXPECT_EQ(dict_col->value_by_value_id(attribute_vector->get(5)), "Bill");
+}
+
+TEST_F(StorageDictionaryColumnTest, ImmutableDictionaryColumn) {
+  auto col = opossum::make_shared_by_column_type<opossum::BaseColumn, opossum::DictionaryColumn>("string", vc_str);
+  auto dict_col = std::dynamic_pointer_cast<opossum::DictionaryColumn<std::string>>(col);
+
+  EXPECT_ANY_THROW(dict_col->append("ohai"));
 }
 
 // TODO(student): You should add some more tests here (full coverage would be appreciated) and possibly in other files.
