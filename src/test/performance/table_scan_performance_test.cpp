2a35c0c7bbe720b332f2eea0c8d882f36743478b
Arthur Silber <arthur@arthursilber.de>
2017-11-27 20:30:20 +0100

    Add empty PerformanceTableScanTest

---
+++ src/test/performance/table_scan_performance_test.cpp
@@ -0,0 +1,13 @@
+#include "../base_test.hpp"
+#include "gtest/gtest.h"
+
+namespace opossum {
+
+class PerformanceTableScanTest : public BaseTest {
+ protected:
+  void SetUp() override {}
+};
+
+TEST_F(PerformanceTableScanTest, EmptyTest) {}
+
+}  // namespace opossum
